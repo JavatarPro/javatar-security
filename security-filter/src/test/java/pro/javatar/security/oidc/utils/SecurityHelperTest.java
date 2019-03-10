ecb3aa32d768e3d8e3240928e5056709069e83c9
Andrii O Murashkin <andrii.murashkin@javatar.pro>
2019-03-10 23:54:01 +0200

    added security helper

---
+++ security-filter/src/test/java/pro/javatar/security/oidc/utils/SecurityHelperTest.java
@@ -0,0 +1,88 @@
+package pro.javatar.security.oidc.utils;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import pro.javatar.security.oidc.model.TokenDetails;
+import pro.javatar.security.oidc.services.OidcConfiguration;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Andrii Murashkin / Javatar LLC
+ * @version 10-03-2019
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class SecurityHelperTest {
+
+    private static final String LOGIN = "user6767";
+    private static final String REALM = "test-realm";
+    private static final String DEFAULT_REALM = "default-realm";
+
+    @Mock
+    private OidcConfiguration oidcConfiguration;
+
+    @InjectMocks
+    private SecurityHelper securityHelper;
+
+    @Test
+    public void getCurrentRealm() {
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken(LOGIN, getTokenDetails(), new ArrayList<>()));
+        assertNotNull(SecurityContextHolder.getContext().getAuthentication());
+
+        assertEquals(REALM, securityHelper.getCurrentRealm());
+    }
+
+    @Test
+    public void getCurrentRealmTokenDetailsIsNull() {
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken(LOGIN, null, new ArrayList<>()));
+        assertNotNull(SecurityContextHolder.getContext().getAuthentication());
+
+        when(oidcConfiguration.getDefaultRealm()).thenReturn(DEFAULT_REALM);
+
+        assertEquals(DEFAULT_REALM, securityHelper.getCurrentRealm());
+    }
+
+    @Test
+    public void getCurrentRealmDefaultRealm() {
+        SecurityContextHolder.clearContext();
+        assertNull(SecurityContextHolder.getContext().getAuthentication());
+
+        when(oidcConfiguration.getDefaultRealm()).thenReturn(DEFAULT_REALM);
+
+        assertEquals(DEFAULT_REALM, securityHelper.getCurrentRealm());
+    }
+
+    @Test
+    public void getCurrentLogin() {
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken(LOGIN, null, new ArrayList<>()));
+        assertNotNull(SecurityContextHolder.getContext().getAuthentication());
+
+        assertEquals(LOGIN, securityHelper.getCurrentLogin());
+    }
+
+    @Test(expected = AccessDeniedException.class)
+    public void getCurrentLoginException() {
+        SecurityContextHolder.clearContext();
+        assertNull(SecurityContextHolder.getContext().getAuthentication());
+
+        securityHelper.getCurrentLogin();
+    }
+
+    private TokenDetails getTokenDetails() {
+        TokenDetails tokenDetails = new TokenDetails();
+        tokenDetails.setRealm(REALM);
+        return tokenDetails;
+    }
+}
